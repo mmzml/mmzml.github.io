f33ee3de8b277e51cb6eb51dc8060428a7b501db
mmzml <muhammad.muzammil7804@gmail.com>
2026-01-07 23:53:50 -0500

    Updated with more experiences

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -11,8 +11,7 @@ const HomePage = () => {
         Toronto pursuing a Specialist in Computer Science and minors in
         Mathematics and Statistics. My current research interests span many
         topics in computer vision (e.g. image segmentation, 3D scene
-        understanding), natural language processing (language generation,
-        applications to healthcare), and machine learning (e.g. generative
+        understanding, applications to healthcare) and machine learning (e.g. generative
         modeling, probabilistic modeling, reinforcement learning). Feel free to
         check out the below information about me and also the{" "}
         <Link to="/research">Projects & Research</Link> tab for more details on
@@ -21,35 +20,90 @@ const HomePage = () => {
 
       <hr />
 
-      <h1>Education</h1>
-      <ul>
-        <li>
-          University of Toronto (2021-): Honours Bachelor of Science
-          <br />
-          CGPA: 3.97 (
-          <a target="_blank" rel="noopener noreferrer" href="/transcript.pdf">
-            Transcript
-          </a>
-          )
-          <ul>
-            <li>
-              Specialist in Computer Science (Focus in Artificial Intelligence)
-            </li>
-            <li>Minor in Mathematics</li>
-            <li>Minor in Statistics</li>
-          </ul>
-        </li>
-        <li>
-          The York School: International Baccalaureate & Ontario Secondary
-          School Diploma
-          <br />
-          IB Score: 41
-        </li>
-      </ul>
-
-      <hr />
-
       <h1>Experiences</h1>
+      <h2>Sep 2025 - Present: Research Student at the{" "}
+        <a target="_blank" rel="noopener noreferrer" href="https://www.koziarskilab.com/">
+          Koziarski Lab
+        </a>
+      </h2>
+      <p>
+        Since September 2025, I am working as a research student at the Koziarski Lab, a joint 
+        lab at{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.sickkids.ca/"
+        >
+          the Hospital for Sick Children
+        </a>{" "}
+        and{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://vectorinstitute.ai/"
+        >
+          Vector Institute
+        </a>
+        , under the supervision of Dr. Michał Koziarski. My research focuses on developing 
+        deep learning models for detecting cellular senescence, with the end goal of 
+        determining the efficacy of different senomorphic drugs. My responsibilities 
+        include data preprocessing, model development, and evaluation. I am excited to 
+        contribute to this important area of research and further develop my skills in 
+        computer vision and biomedical applications.
+      </p>
+      <h2>
+        May 2025 - Present: Research Assistant for Professor Babak Taati
+      </h2>
+      <p>
+        From May to August 2025, I worked with Professor Babak Taati as an undergraduate 
+        research assistant as a{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://artsci.calendar.utoronto.ca/course/csc494h1"
+        >
+          CSC495
+        </a>{" "}
+        student. My work involved investigating the impact of a synthetic dataset,{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://borealisdata.ca/dataset.xhtml?persistentId=doi%3A10.5683%2FSP3%2FWCXMAP"
+        >
+          SynPAIN
+        </a>
+        , on the performance of a pain detection model for older adults with dementia. 
+        This included data preprocessing, model training, and evaluation. I also 
+        contributed to drafting and submitting a{" "}
+        <a
+          href="https://arxiv.org/abs/2507.19673"
+          target="_blank"
+          rel="noopener no referrer"
+        >
+          research paper
+        </a>{" "}
+        based on our findings. I also worked on two other projects with Prof. Taati's research group, both
+        of which resulted in publications. More details can be found in the{" "}
+        <Link to="/research">Projects & Research</Link> page. I am currently continuing 
+        my research with Prof. Taati as a Research Assistant on a volunteer basis.
+      </p>
+      <h2>
+        Sep 2025 - Dec 2025: Teaching Assistant for{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://artsci.calendar.utoronto.ca/course/csc207h1"
+        >
+          CSC207
+        </a>
+      </h2>
+      <p>
+        From September to December 2025, I was a TA for CSC207: Software
+        Design. My responsibilites included holding weekly office hours to 
+        assist students in their understanding of the material, while also 
+        serving as a midterm invigilator, marking midterms & final exams.
+        I also viewed and graded final project presentations.
+      </p>
       <h2>
         Jan 2025 - Apr 2025: Teaching Assistant for{" "}
         <a
@@ -61,10 +115,10 @@ const HomePage = () => {
         </a>
       </h2>
       <p>
-        Form January to April 2025, I was a Lab TA for CSC148: Introduction to
+        From January to April 2025, I was a TA for CSC148: Introduction to
         Computer Science. My responsibilites included leading two weekly
         two-hour lab sessions, while also holding office hours, serving as an
-        exam invigilator, and marking the midterm & final exam.
+        exam invigilator, and marking midterms & final exams.
       </p>
       <h2>
         May 2024 - Dec 2024: Hanabi project (
@@ -145,6 +199,34 @@ const HomePage = () => {
         for a diverse student body. This experience deepened my skills in
         leadership, event planning, and community building.
       </p>
+
+      <hr />
+
+      <h1>Education</h1>
+      <ul>
+        <li>
+          University of Toronto (2021-): Honours Bachelor of Science
+          <br />
+          CGPA: 3.97 (
+          <a target="_blank" rel="noopener noreferrer" href="/transcript.pdf">
+            Transcript
+          </a>
+          )
+          <ul>
+            <li>
+              Specialist in Computer Science (Focuses in Artificial Intelligence and Computer Vision)
+            </li>
+            <li>Minor in Mathematics</li>
+            <li>Minor in Statistics</li>
+          </ul>
+        </li>
+        <li>
+          The York School: International Baccalaureate & Ontario Secondary
+          School Diploma
+          <br />
+          IB Score: 41
+        </li>
+      </ul>
 
       <hr />
 
